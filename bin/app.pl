6566477009b9de9f8adf5232d2cf7d23f47ff2ab
Sean Zellmer <sean@lejeunerenard.com>
2013-02-07 18:40:35 -0600

    Added Vanilla Dancer App with DBIx Plugins

---
+++ bin/app.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/env perl
+use lib "local/lib/perl5/";
+use Dancer;
+use Choreographer_UI;
+dance;
---
+++ lib/Choreographer_UI.pm
@@ -0,0 +1,10 @@
+package Choreographer_UI;
+use Dancer ':syntax';
+
+our $VERSION = '0.1';
+
+get '/' => sub {
+    template 'index';
+};
+
+true;
